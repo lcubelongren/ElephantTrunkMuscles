ecbcfffc3eb8550dd2f9aa290eb952f38c266ea1
Luke Longren <lcubelongren@gmail.com>
2022-08-07 21:24:27 +0200

    added dimensionality reduction

--- plotting.py
+++ plotting.py
@@ -2,14 +2,25 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+import seaborn as sns
+from sklearn.decomposition import PCA, KernelPCA
 import itertools
 
 
 workingDir = 'D:\Luke\ElephantTrunkMuscles'
 
 
-def plotMuscleAnalysis(totalmuscleNum, lengths, surface_areas, volumes):
+def plotMetricsAll(df):
+    totalmuscleNum = len(df['index'])
+    normalized_df = (df - df.min()) / (df.max() - df.min())
+    ax = sns.stripplot(data=normalized_df.drop(columns=['index']), size=1)
+    ax.set_title('Individual muscle measurements - $n={}$'.format(totalmuscleNum))
+    ax.set_ylabel('normalized value')
+    fig = ax.get_figure()
+    fig.savefig(workingDir + '\plotting_output\MetricsAll.png')
+
+
+def plotMetricsSize(totalmuscleNum, lengths, surface_areas, volumes):
     fig, axs = plt.subplots(ncols=3, figsize=(12,4))
     c, s = 'k', 3
     names = ['length', 'surface area', 'volume']
@@ -27,15 +38,39 @@ def plotMuscleAnalysis(totalmuscleNum, lengths, surface_areas, volumes):
     plt.suptitle('Individual muscle measurements - $n={}$'.format(totalmuscleNum))
     plt.tight_layout()
     plt.subplots_adjust(top=0.9)
-    plt.savefig(workingDir + '\plotting_output\MuscleAnalysis.png')
-    
-    
+    plt.savefig(workingDir + '\plotting_output\MetricsSize.png')
+
+
+def plotMetricsDR(df):
+    totalmuscleNum = len(df['index'])
+    normalized_df = (df - df.min()) / (df.max() - df.min())
+    X = normalized_df.drop(columns=['index', 'OrientationPhi'])
+    pca = PCA(n_components=2)
+    X_pca = pca.fit_transform(X)
+    kpca = KernelPCA(n_components=None, kernel='rbf', gamma=10, fit_inverse_transform=True, alpha=0.1)
+    X_kpca = kpca.fit_transform(X)
+    fig, axs = plt.subplots(ncols=2, figsize=(8,4))
+    axs[0].scatter(X_pca[:,0], X_pca[:,1], s=2)
+    axs[0].set_title('PCA')
+    axs[1].scatter(X_kpca[:,0], X_kpca[:,1], s=2)
+    axs[1].set_title('Kernel PCA')
+    [axs[i].set_xlabel('$PC_1$') for i in range(2)]
+    [axs[i].set_ylabel('$PC_2$') for i in range(2)]
+    [axs[i].axis('square') for i in range(2)]
+    plt.suptitle('Dimensionality reduction - $n={}$'.format(totalmuscleNum))
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.85, bottom=0.2)
+    plt.figtext(0.5, 0.03, 'Included metrics: ' + str(X.columns.values), 
+                ha='center', fontsize=9, bbox={'alpha': 0.5, 'pad': 5})
+    plt.savefig(workingDir + '\plotting_output\MetricsDR.png')
+
+
 df_fname = workingDir + r'\analysis_output\muscles_p1-combined.Label-Analysis.csv'
 df = pd.read_csv(df_fname, skiprows=1).drop(columns=['Materials'])
 df = df[np.isfinite(df).all(True)]  # remove rows with inf
-plotMuscleAnalysis(len(df['index']), df['Length3d'], df['VoxelFaceArea'], df['Volume3d'])
-
-
+plotMetricsAll(df)
+plotMetricsSize(len(df['index']), df['Length3d'], df['VoxelFaceArea'], df['Volume3d'])
+plotMetricsDR(df)
 
 
 
